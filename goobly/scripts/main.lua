2f4e71bac5f9bc64beb9ccf5f6f20964da98e282
Lobster Man <totallynotlondon@gmail.com>
2024-09-09 16:26:58 -0500

    working on a goobly package

---
+++ goobly/scripts/main.lua
@@ -0,0 +1,3 @@
+Scene:reset();
+local spawn_goobly = require('./packages/@frosoto/goobly/lib/goobly.lua');spawn_goobly(vec2(-1, -9));
+local spawn_joblo = require('./packages/@frosoto/goobly/lib/joblo.lua');spawn_joblo(vec2(1, -9))
